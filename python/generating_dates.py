63bdcd4e3a548381057ad6bb5d935cead97f279a
Adam Shelbourne <a.shelbourne@lancaster.ac.uk>
2017-10-12 22:14:16 +0100

    fixed js generation

--- python/generating_dates.py
+++ python/generating_dates.py
@@ -1,66 +1,25 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[50]:
 
 # Python version
 import sys
-print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
-import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
-
 # In addition to the online example, these packages below help dealinf with the file system
 import glob
 import os
-from PIL import Image
 
 
-# In[7]:
+# In[3]:
 
-## Part 1: Lets do a loop
-
-
-# In[4]:
-
-# Load libraries
-import pandas
-from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from datetime import date
-
-
-# In[5]:
-
-dir = r'../data/*.dat'
+dir = '../data/*'
 names = ['UTCDateTime', 'LocalDateTime', 'Temperature', 'Counts', 'Frequency', 'MSAS']
+print(glob.glob(dir))
 
 
 # In[9]:
 
-# Set the common time grid onto which each night is sampled. The common grid 
+# Set the common time grid onto which each night is sampled. The common grid
 # is defined by the start time defined below, and the number of hours from that time
 beginh = 16 # Hour of start time (hour)
 beginm = 30 # Hour of start time (minute)
@@ -68,17 +27,60 @@ nhrs = 15   # Number of hours to extend time grid
 
 #print  (glob.glob(r'C:\\Users\\Adam Shelbourne\\Uni\\Physics\\Astr310\\Python\\mu\\daily\\20*.dat'))
 
-numbfile = len(glob.glob(r'../data/20*.dat'))
+numbfile = len(glob.glob('../data/20*.dat'))
 print (numbfile)
 
 
-# In[8]:
+# In[59]:
 
-for infile in sorted(glob.glob(r'../data/20*.dat')):
-   print (infile[69:77])
+javascript= '../html/js/darkskiesdata.js'
+f2=open(javascript,'w')
+#Write Header first 7 lines
+f2.write(str("Vue.use(VueEventCalendar.default, {locale: \'en\', color: \'#4fc08d\'}) \n \
+new Vue({\n \
+  el: \'#example\',\n \
+  data: function () { \n\
+    return {\n \
+      demoEvents:\n\
+	  [\n\n"))
+#loop through and write to file
+for infile in sorted(glob.glob('../data/20*.dat')):
+
+
+   currentFile = os.path.basename(infile)
+
+
+   f2.write(str("{\n" ))
+   #print (("date:" + "\'" + (currentFile[0:4])+ "/" + (currentFile[4:6]) + "/" + (currentFile[6:8])+ "\'"))
+   f2.write(str(("date:" + "\'" + (currentFile[0:4])+ "/" + (currentFile[4:6]) + "/" + (currentFile[6:8])+ "\',\n")))
+   f2.write(str(("title: \'Macquarie Uni\',\n" )))
+   f2.write(str(("desc: \'" + currentFile +"'\n" )))
+   f2.write(str("},\n" ))
+
 
 
 # In[ ]:
 
+#
 
 
+# In[54]:
+
+#Write my block
+
+f2.write(str("\n{\n" ))
+f2.write(str(("date: \'1996/12/12\',")))
+f2.write(str(("title: \'A legend was born\',")))
+f2.write(str(("desc: \'Well done for finding my easter egg!\'")))
+f2.write(str("}\n\n" ))
+#Write footer last 4 lines
+
+f2.write(str("            ]\n"))
+f2.write(str("        }\n"))
+f2.write(str("    }\n"))
+f2.write(str("  }\n);\n"))
+
+f2.close()
+
+
+# In[ ]:
